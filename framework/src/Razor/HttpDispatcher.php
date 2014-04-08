04887af746c1cab97734e792d2a5d6fe245ebae5
Nick Rawe <nickrawe2@gmail.com>
2014-04-08 12:57:20 +0100

    Added the HTTP Dispatcher
    For development mode this will allow us to link HTTP methods to handlers
    on a Controller.

---
+++ framework/src/Razor/HttpDispatcher.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Razor;
+
+use Rawebone\Injector\Injector;
+
+/**
+ * Handles dispatching
+ */
+class HttpDispatcher
+{
+    protected $injector;
+    protected $resolver;
+
+    public function __construct(Injector $injector, ServiceResolver $resolver)
+    {
+        $this->injector = $injector;
+        $this->resolver = $resolver;
+    }
+
+    public function dispatch(Controller $controller)
+    {
+        // Prevent modification to services inside of a controller
+        // as such behaviour will make it difficult to in-line later.
+        $key = $this->resolver->lock();
+
+        /** @var \Symfony\Component\HttpFoundation\Request $request */
+        $request = $this->injector->service("request");
+        $method  = strtolower($request->getMethod());
+
+        try {
+            if (($handler = $controller->$method) !== null) {
+                $this->injector->inject($handler);
+            } else {
+                $this->injector->inject($controller->notFound);
+            }
+
+        } catch (\Exception $ex) {
+
+            // Allow the error handler to access the raised exception as a service
+            $this->resolver->unlock($key);
+            $this->resolver->registerService("exception", function () use ($ex) { return $ex; });
+            $key = $this->resolver->lock();
+
+            $this->injector->inject($controller->error);
+        }
+
+        $this->resolver->unlock($key);
+    }
+}

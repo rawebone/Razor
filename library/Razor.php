03c05f739a9cd31685ab978baf9dfceeb3af7a17
Nick Rawe <rawebone@gmail.com>
2014-07-02 23:31:46 +0100

    Updated Documentation on the Razor class

--- library/Razor.php
+++ library/Razor.php
@@ -7,6 +7,12 @@ class Razor
 	protected static $environment;
 	protected static $endPoint;
 
+	/**
+	 * Returns or sets the current environment.
+	 *
+	 * @param Environment $environment
+	 * @return Environment
+	 */
 	public static function environment(Environment $environment = null)
 	{
 		if ($environment) {
@@ -20,11 +26,21 @@ class Razor
 		return self::$environment;
 	}
 
+	/**
+	 * Returns the last End Point that was ran.
+	 *
+	 * @return EndPoint|null
+	 */
 	public static function endPoint()
 	{
 		return static::$endPoint;
 	}
 
+	/**
+	 * Runs the EndPoint.
+	 *
+	 * @param EndPoint $endPoint
+	 */
 	public static function run(EndPoint $endPoint)
 	{
 		static::$endPoint = $endPoint;
